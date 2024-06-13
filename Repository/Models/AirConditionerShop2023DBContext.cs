280d0e130c3ec768e064d72177313a81c3f50060
CatCoolKit <caonamhai8262@gmail.com>
2024-06-14 02:00:12 +0700

    update 1

---
+++ Repository/Models/AirConditionerShop2023DBContext.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+
+#nullable disable
+
+namespace Repository.Models
+{
+    public partial class AirConditionerShop2023DBContext : DbContext
+    {
+        public AirConditionerShop2023DBContext()
+        {
+        }
+
+        public AirConditionerShop2023DBContext(DbContextOptions<AirConditionerShop2023DBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<AirConditioner> AirConditioners { get; set; }
+        public virtual DbSet<StaffMember> StaffMembers { get; set; }
+        public virtual DbSet<SupplierCompany> SupplierCompanies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer(GetConnectionString());
+            }
+        }
+        private string GetConnectionString()
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", true, true)
+                        .Build();
+            var strConn = config["ConnectionStrings:DefaultConnectionStringDB"];
+
+            return strConn;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<AirConditioner>(entity =>
+            {
+                entity.ToTable("AirConditioner");
+
+                entity.Property(e => e.AirConditionerId).ValueGeneratedNever();
+
+                entity.Property(e => e.AirConditionerName)
+                    .IsRequired()
+                    .HasMaxLength(200);
+
+                entity.Property(e => e.FeatureFunction).HasMaxLength(250);
+
+                entity.Property(e => e.SoundPressureLevel).HasMaxLength(80);
+
+                entity.Property(e => e.SupplierId).HasMaxLength(20);
+
+                entity.Property(e => e.Warranty).HasMaxLength(60);
+
+                entity.HasOne(d => d.Supplier)
+                    .WithMany(p => p.AirConditioners)
+                    .HasForeignKey(d => d.SupplierId)
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .HasConstraintName("FK__AirCondit__Suppl__3C69FB99");
+            });
+
+            modelBuilder.Entity<StaffMember>(entity =>
+            {
+                entity.HasKey(e => e.MemberId)
+                    .HasName("PK__StaffMem__0CF04B380A68D017");
+
+                entity.ToTable("StaffMember");
+
+                entity.HasIndex(e => e.EmailAddress, "UQ__StaffMem__49A147409D4397C4")
+                    .IsUnique();
+
+                entity.Property(e => e.MemberId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("MemberID");
+
+                entity.Property(e => e.EmailAddress).HasMaxLength(60);
+
+                entity.Property(e => e.FullName)
+                    .IsRequired()
+                    .HasMaxLength(60);
+
+                entity.Property(e => e.Password).IsUnicode(false);
+            });
+
+            modelBuilder.Entity<SupplierCompany>(entity =>
+            {
+                entity.HasKey(e => e.SupplierId)
+                    .HasName("PK__Supplier__4BE666B480666A70");
+
+                entity.ToTable("SupplierCompany");
+
+                entity.Property(e => e.SupplierId).HasMaxLength(20);
+
+                entity.Property(e => e.PlaceOfOrigin).HasMaxLength(60);
+
+                entity.Property(e => e.SupplierDescription).HasMaxLength(250);
+
+                entity.Property(e => e.SupplierName)
+                    .IsRequired()
+                    .HasMaxLength(80);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Repository/Models/SupplierCompany.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Repository.Models
+{
+    public partial class SupplierCompany
+    {
+        public SupplierCompany()
+        {
+            AirConditioners = new HashSet<AirConditioner>();
+        }
+
+        public string SupplierId { get; set; }
+        public string SupplierName { get; set; }
+        public string SupplierDescription { get; set; }
+        public string PlaceOfOrigin { get; set; }
+
+        public virtual ICollection<AirConditioner> AirConditioners { get; set; }
+    }
+}
