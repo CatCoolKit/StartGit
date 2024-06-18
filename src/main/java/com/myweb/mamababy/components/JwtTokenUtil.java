e5b718c030d245599c893243e059f0005277d514
CatCoolKit <caonamhai8262@gmail.com>
2024-06-18 17:09:56 +0700

    update

--- src/main/java/com/myweb/mamababy/components/JwtTokenUtil.java
+++ src/main/java/com/myweb/mamababy/components/JwtTokenUtil.java
@@ -1,4 +1,5 @@
 package com.myweb.mamababy.components;
+
 import com.myweb.mamababy.exceptions.InvalidParamException;
 import com.myweb.mamababy.models.User;
 import io.jsonwebtoken.Claims;
@@ -21,43 +22,43 @@ import java.util.function.Function;
 @RequiredArgsConstructor
 public class JwtTokenUtil {
     @Value("${jwt.expiration}")
-    private int expiration; //save to an environment variable
+    private int expiration; // save to an environment variable
     @Value("${jwt.secretKey}")
     private String secretKey;
 
-    public String generateToken(User user) throws Exception{
-        //properties => claims
+    public String generateToken(User user) throws Exception {
+        // properties => claims
         Map<String, Object> claims = new HashMap<>();
-        //this.generateSecretKey();
+        // this.generateSecretKey();
         claims.put("username", user.getUsername());
         try {
             String token = Jwts.builder()
-                    .setClaims(claims) //how to extract claims from this ?
+                    .setClaims(claims) // how to extract claims from this ?
                     .setSubject(user.getUsername())
                     .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000L))
                     .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                     .compact();
             return token;
-        }catch (Exception e) {
-            //you can "inject" Logger, instead System.out.println
-            throw new InvalidParamException("Cannot create jwt token, error: "+e.getMessage());
-            //return null;
+        } catch (Exception e) {
+            // you can "inject" Logger, instead System.out.println
+            throw new InvalidParamException("Cannot create jwt token, error: " + e.getMessage());
+            // return null;
         }
     }
+
     private Key getSignInKey() {
         byte[] bytes = Decoders.BASE64.decode(secretKey);
-        //Keys.hmacShaKeyFor(Decoders.BASE64.decode("TaqlmGv1iEDMRiFp/pHuID1+T84IABfuA0xXh4GhiUI="));
+        // Keys.hmacShaKeyFor(Decoders.BASE64.decode("TaqlmGv1iEDMRiFp/pHuID1+T84IABfuA0xXh4GhiUI="));
         return Keys.hmacShaKeyFor(bytes);
     }
 
-
-//    private String generateSecretKey() {
-//        SecureRandom random = new SecureRandom();
-//        byte[] keyBytes = new byte[32]; // 256-bit key
-//        random.nextBytes(keyBytes);
-//        String secretKey = Encoders.BASE64.encode(keyBytes);
-//        return secretKey;
-//    }
+    // private String generateSecretKey() {
+    // SecureRandom random = new SecureRandom();
+    // byte[] keyBytes = new byte[32]; // 256-bit key
+    // random.nextBytes(keyBytes);
+    // String secretKey = Encoders.BASE64.encode(keyBytes);
+    // return secretKey;
+    // }
     private Claims extractAllClaims(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(getSignInKey())
@@ -65,21 +66,26 @@ public class JwtTokenUtil {
                 .parseClaimsJws(token)
                 .getBody();
     }
-    public  <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = this.extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
-    //check expiration
+
+    // check expiration
     public boolean isTokenExpired(String token) {
         Date expirationDate = this.extractClaim(token, Claims::getExpiration);
         return expirationDate.before(new Date());
     }
+
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
+
     public boolean validateToken(String token, UserDetails userDetails) {
         String username = extractUsername(token);
         return (username.equals(userDetails.getUsername()))
                 && !isTokenExpired(token);
     }
+
 }
